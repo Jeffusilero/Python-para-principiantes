18c89270ee5b8a92f3549e49699fa37f2b63172e
Jeffusilero <jeffersonandres2002@hotmail.com>
2024-02-05 16:39:21 -0500

    Updated

--- Reto 30 dias con python/04_lists.py
+++ Reto 30 dias con python/04_lists.py
@@ -15,5 +15,17 @@ my_other_list = [29, 1.65, "Jefferson", "Lascano"]
 print(type(my_list))
 print(type(my_other_list))
 
+print(my_other_list[0])
+print(my_other_list[1])
 print(my_other_list[-1])
+print(my_other_list[-4])
+print(my_list.count(30)) # cuantas veces se retiro el valor
+# print(my_other_list[4]) IndexError
+# print(my_other_list[-4]) IndexError
 
+age, height, name, surname = my_other_list
+print(name)
+
+name, height, age, surname = my_other_list[]
+
+print()
