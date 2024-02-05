0e8e651ebef9de046dff5cc0865c29fa1cad7484
Jeffusilero <jeffersonandres2002@hotmail.com>
2024-02-05 16:15:02 -0500

    Updated

---
+++ Reto 30 dias con python/04_lists.py
@@ -0,0 +1,19 @@
+# lists # 
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list =[29, 24, 62, 52, 30, 30, 17]
+
+print(my_list)
+print(len(my_list)) # cuenta la cantidad de caracteres en la lista
+
+my_other_list = [29, 1.65, "Jefferson", "Lascano"]
+
+print(type(my_list))
+print(type(my_other_list))
+
+print(my_other_list[-1])
+
