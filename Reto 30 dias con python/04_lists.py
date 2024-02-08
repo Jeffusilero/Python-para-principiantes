0b1352c33c902952f22377cb9f53da6726316937
jeffersonandres2002@hotmail.com <jeffersonandres2002@hotmail.com>
2024-02-07 19:08:51 -0500

    Updated

--- Reto 30 dias con python/04_lists.py
+++ Reto 30 dias con python/04_lists.py
@@ -35,10 +35,13 @@ print(my_list + my_other_list)
 my_other_list.append("Jeffusilero") #agg nuevo elemento al final
 print(my_other_list)
 
-my_other_list.insert(1, "Negro") # se agg en la prosicion que indicamos 
+my_other_list.insert(1, "Plomo") # se agg en la prosicion que indicamos 
 print(my_other_list)
 
-my_other_list.remove("Negro") # elimina el elemento que se detalla
+my_other_list[1] = "Negro" #modifica el valor del indice de acorde a lo que agg
+print(my_other_list)
+
+my_other_list.remove("Negro") # elimina el elemento que se detalla en el parentesis
 print(my_other_list)
 
 my_list.remove(30) # elimina el elemento que se detalla
@@ -47,6 +50,24 @@ print(my_list)
 my_list.pop() # elimina el ultimo elemento de la lista 
 print(my_list)
 
+my_pop_element = my_list.pop(2)
+print(my_pop_element) # elimina el segundo elemento de la lista
+print(my_list)
+
+del my_list[2] #elimina definitvamente el elemento seleccionado
+print(my_list)
+
+my_new_list =my_list.copy() #copia los elemento de la lista anterior
+
+my_list.clear() #elimina todo los valores de lista
+print(my_list)
+print(my_new_list)
+
+my_new_list.reverse() #imprime los valores de lista al reves
+print(my_new_list)
+
+my_new_list.sort() #orden los valores de indice de menor a mayor
+print(my_new_list)
 
 my_list= "hola python"
 print(my_list)
